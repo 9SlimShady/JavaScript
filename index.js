4ecfa1fc4e22a535015e44541aa864a41c4cdbe7
9SlimShady <90679621+9SlimShady@users.noreply.github.com>
2022-03-22 17:25:57 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("click").style.color = "red";
+
+
+let arr = [30];
+
+// array.forEach(function(element)  {
+//     console.log (element)
+// });
+for (let i = 0; i<arr.length;i++){
+    console.log(arr[i])
+}
+
+
+const string = "this is me rehman";
+
+console.log(string.slice(6))
+
+let date = new Date();
+
+console.log(date.getMinutes())
